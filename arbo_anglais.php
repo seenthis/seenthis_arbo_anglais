fbb72ecc9a17dc32007a62f7ef4a42be9fd13368
JamesRezo <james@rezo.net>
2024-07-06 18:39:54 +0200

    chore!:standard files & stable version
    BREAKING CHANGE: SPIP 4.1 mini

--- arbo_anglais.php
+++ arbo_anglais.php
@@ -1,28 +1,8 @@
 <?php
 
 function arbo_anglais_traduire($var) {
-
-	$var["data"] = ereg_replace("^auteur", "people", $var["data"]);
-	$var["data"] = ereg_replace("^mot", "tags", $var["data"]);
+	$var['data'] = preg_replace(',^auteur,', 'people', $var['data']);
+	$var['data'] = preg_replace(',^mot,', 'tags', $var['data']);
 
 	return $var;
-
 }
-
-/*
-
-(
-    [data] => auteur/arno
-    [objet] => Array
-        (
-            [url] => 
-            [date] => 
-            [titre] => ARNO*
-            [type_parent] => 
-            [type] => auteur
-            [id_objet] => 1
-        )
-
-)
-*/
-?>
